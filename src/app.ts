ec1f5a0adf847870ba86aa371aa1f7b6865e1d82
spde3289 <spde3289@naver.com>
2024-04-19 19:47:25 +0900

    feat: 유저 id 값 추가

--- src/app.ts
+++ src/app.ts
@@ -18,37 +18,30 @@ const sever = app.listen("3000", () => {
 
 let io = new Server(sever);
 
-let userList: any[] | any;
-let room: any;
+let userList: any[] = [];
 
 io.on("connection", (socket) => {
   console.log("접속");
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
-
     for (let i = 0; i < userList?.length; i++) {
-      if (userList[i] === socket.id) {
+      if (userList[i].id === socket.id) {
         userList.splice(i, 1);
         i--;
       }
     }
-    io.to(room).emit("user list", userList);
+    io.emit("user list", userList);
   });
 
-  socket.on("join room", (roomId) => {
-    room = roomId;
-
-    userList = roomList.find((el) => el.name === roomId)?.userList;
-
-    socket.join(roomId);
-    userList.push(socket.id);
-    io.to(room).emit("user list", userList);
+  socket.on("join room", (user) => {
+    userList.push({ user: user, id: socket.id });
+    io.emit("user list", userList);
   });
 
   socket.on("chat message", (msg, id) => {
     console.log(id + ": " + msg);
-    io.to(room).emit("chat message", { msg: msg, user: id });
+    io.emit("chat message", { msg: msg, user: id });
   });
 });
 
