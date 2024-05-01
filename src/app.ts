575a58fe350e841164849e7d1985469f6b407906
spde3289 <spde3289@naver.com>
2024-05-01 22:03:56 +0900

    feat: 채팅방 입장 및 채팅 기록 로깅

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,9 @@ import { Server } from "socket.io";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-import chatLog from "./data/chatLog.json";
 import indexRouter from "./router/index";
 import roomRouter from "./router/room";
 import { UserListType, ChatLogType } from "./Type";
-import { time } from "console";
 
 const app = express();
 const sever = app.listen("3000", () => {
@@ -21,36 +19,124 @@ const sever = app.listen("3000", () => {
 
 let io = new Server(sever);
 
-let userList: UserListType = [];
+const Announcement = "공지";
+const users: any = [];
+
+function userJoin(id: any, username: any, room: any) {
+  const user = { id, username, room };
+  users.push(user);
+  return user;
+}
+
+function getCurrentUser(id: any) {
+  return users.find((user: any) => user.id === id);
+}
+
+function userLeave(id: any) {
+  const index = users.findIndex((user: any) => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+}
+
+function getRoomUsers(room: any) {
+  return users.filter((user: any) => user.room === room);
+}
+
+function fromatMessage(name: any, text: any, room: any) {
+  const date = new Date().toString();
+
+  const filePath = `./src/data/${room}-chatLog.json`;
+  fs.readFile(filePath, "utf8", (err: any, data: any) => {
+    if (err) {
+      console.error("파일을 읽는 도중 오류가 발생했습니다:", err);
+      return;
+    }
+
+    // JSON 파싱
+    let jsonData;
+    try {
+      jsonData = JSON.parse(data);
+    } catch (parseErr) {
+      console.error("JSON을 파싱하는 도중 오류가 발생했습니다:", parseErr);
+      return;
+    }
+
+    jsonData.push({ text: text, name: name, time: date });
+
+    // 수정된 JSON 파일 쓰기
+    fs.writeFile(
+      filePath,
+      JSON.stringify(jsonData, null, 2),
+      "utf8",
+      (writeErr) => {
+        if (writeErr) {
+          console.error("파일을 쓰는 도중 오류가 발생했습니다:", writeErr);
+          return;
+        }
+      }
+    );
+  });
+
+  return {
+    name,
+    text,
+    time: date,
+  };
+}
+
+function chatLog(room: any) {
+  const filePath = `./src/data/${room}-chatLog.json`;
+  const jsonData = fs.readFileSync(filePath, "utf8");
+
+  return jsonData;
+}
 
 io.on("connection", (socket) => {
-  // 연결 끊김
-  socket.on("disconnect", () => {
-    for (let i = 0; i < userList?.length; i++) {
-      if (userList[i].id === socket.id) {
-        userList.splice(i, 1);
-        i--;
-      }
-    }
-    io.emit("user list", userList);
-  });
-
   // 방 입장
-  socket.on("join room", (user) => {
-    userList.push({ user: user, id: socket.id });
-    io.emit("user list", userList);
+  socket.on("join room", (username, room) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
+
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "chat message",
+        fromatMessage(
+          Announcement,
+          `${user.username}님이 입장하셨습니다.`,
+          user.room
+        )
+      );
+
+    io.to(user.room).emit("chat log", chatLog(user.room));
+
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
   });
 
-  // 채팅 입력
-  socket.on("chat message", (msg, id) => {
-    const ChatLog: ChatLogType = chatLog;
-    const date = new Date().toString();
+  socket.on("chat message", (msg) => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit(
+      "chat message",
+      fromatMessage(user.username, msg, user.room)
+    );
+  });
 
-    ChatLog.push({ msg: msg, user: id, time: date });
-    const stringJson = JSON.stringify(ChatLog);
-
-    fs.writeFileSync("./src/data/chatLog.json", stringJson);
-    io.emit("chat message", { msg: msg, user: id });
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        "chat message",
+        fromatMessage(
+          Announcement,
+          `${user.username}님이 퇴장하셨습니다.`,
+          user.room
+        )
+      );
+    }
   });
 });
 
@@ -60,10 +146,9 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
-  bodyParser.json();
   next();
 });
-
+app.use(bodyParser.json());
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use("/", indexRouter);
 app.use("/room", roomRouter);
--- src/router/room/index.ts
+++ src/router/room/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
+import fs from "fs";
 import roomList from "../../data/roomList.json";
 
 let router = express.Router();
@@ -6,6 +7,20 @@ let router = express.Router();
 /** 방 리스트 */
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.json(roomList);
+  // res.json({ user: "tj" });
+});
+
+router.post("/", (req: Request, res: Response, next: NextFunction) => {
+  // console.log(req);
+  // console.log(res)
+  const postData = req.body;
+  const capyRoomList = roomList;
+  // capyRoomList.push(postData);
+
+  const stringJson = JSON.stringify(capyRoomList);
+  // fs.writeFileSync("./src/data/roomList.json", stringJson);
+  // io.emit("chat message", { msg: msg, user: id });
+  res.send("POST request to the homepage");
 });
 
 export default router;
