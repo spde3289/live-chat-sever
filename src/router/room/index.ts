d3369eda4e6fdfbf7c0b5f2605243f48ba8c614b
spde3289 <spde3289@naver.com>
2024-05-07 19:00:19 +0900

    feat: 채팅방 생성 api

--- src/router/room/index.ts
+++ src/router/room/index.ts
@@ -6,21 +6,27 @@ let router = express.Router();
 
 /** 방 리스트 */
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
-  console.log("dddd")
+  // console.log("dddd")
   res.json(roomList);
   // res.json({ user: "tj" });
 });
 
 router.post("/", (req: Request, res: Response, next: NextFunction) => {
-  // console.log(req);
-  // console.log(res)
   const postData = req.body;
+  console.log(postData);
   const capyRoomList = roomList;
-  // capyRoomList.push(postData);
+  if (postData) {
+    capyRoomList.push(postData);
+  }
 
   const stringJson = JSON.stringify(capyRoomList);
-  // fs.writeFileSync("./src/data/roomList.json", stringJson);
-  // io.emit("chat message", { msg: msg, user: id });
+
+  const Json = JSON.stringify([]);
+
+  fs.writeFileSync(`./src/data/${postData.name}-chatLog.json`, Json);
+
+  console.log(stringJson);
+  fs.writeFileSync("./src/data/roomList.json", stringJson);
   res.send("POST request to the homepage");
 });
 
