e2b2fa96cfb50c5d1b7f34a7b63c16ec791df09e
spde3289 <spde3289@naver.com>
2024-05-09 22:03:56 +0900

    fix: req 데이터 에러 수정

--- src/router/room/index.ts
+++ src/router/room/index.ts
@@ -9,20 +9,30 @@ router.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.json(roomList);
 });
 
-interface RoomData {
+interface ReqBodyType {
   id: string;
   name: string;
   user: string;
   status: string;
 }
 
+interface RoomDataType {
+  id: string;
+  roomName: string;
+  status: string;
+}
+
 router.post("/", (req: Request, res: Response, next: NextFunction) => {
-  const postData: RoomData = req.body;
-  // console.log(postData);
+  const postData: ReqBodyType = req.body;
   const date = new Date().toString();
-  const capyRoomList: RoomData[] = roomList;
+  console.log(postData);
+  const capyRoomList: RoomDataType[] = roomList;
   if (Array.isArray(capyRoomList) && typeof postData === "object") {
-    capyRoomList.push(postData);
+    capyRoomList.push({
+      id: postData.id,
+      roomName: postData.name,
+      status: "ongoing",
+    });
   }
 
   const stringJson = JSON.stringify(capyRoomList);
